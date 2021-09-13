4a53ef0a36ec6b9f7b97c38b71c34ee85ce314ae
tikkasandy <tikkasandy@gmail.com>
2021-09-13 17:16:56 +0300

    Make edits task 03

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -13,37 +13,18 @@ const images = [
   },
 ];
 
-// Make Gallery
+const gallery = document.querySelector('#gallery');
+gallery.style.listStyle = "none";
+gallery.style.display = "flex";
+gallery.style.justifyContent = "space-around";
+gallery.style.padding = "0";
+
 const createGalleryItem = items => {
   return items.map(({ url, alt }) => {
-    return `<li><img src="${url}" alt="${alt}"</li>`
+    return `<li><img src="${url}" alt="${alt}" style="width: 400px; height: 250px; object-fit: cover;"></li>`
   }).join("");
 }
 
 const galleryEl = document.querySelector('#gallery');
 galleryEl.insertAdjacentHTML("beforeend", createGalleryItem(images));
 
-// Add classes
-galleryEl.classList.add("gallery");
-
-const itemsEl = galleryEl.querySelectorAll('li');
-itemsEl.forEach(element => element.classList.add("gallery__item"));
-
-const imgEl = galleryEl.querySelectorAll('img');
-imgEl.forEach(element => element.classList.add("gallery__img"));
-
-//Add styles
-const galleryStyle = document.querySelectorAll('.gallery');
-galleryStyle.forEach(element => {
-  element.style.listStyle = "none";
-  element.style.display = "flex";
-  element.style.justifyContent = "space-around";
-  element.style.padding = "0";
-});
-
-const galleryImgStyle = document.querySelectorAll('.gallery__img');
-galleryImgStyle.forEach(element => {
-  element.style.width = "400px";
-  element.style.height = "250px";
-  element.style.objectFit = "cover";
-});
